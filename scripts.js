20234f8a82ea85eeba73905b688b4e51bb31e904
ahmed <ahmed_u2@live.com>
2021-02-07 18:57:07 -0500

    Added ability to display books, take form data as input, and new book button

--- scripts.js
+++ scripts.js
@@ -7,22 +7,56 @@ function Book(title, author, pages, read){
     this.read = read;
     
     this.info = function() {
-      return "" +title+ ", " +author+ ", " +pages+ ", " +read;
+      (read) ? read = 'read' : read = 'not read';
+      return "Title: " +title+ " Author: " +author+ " Pages: " +pages+ " Read: " +read;
     }
   }
 
 function addBookToLibrary(){
-    // Take user input and store information in book array as object.
-  
-    bookName = prompt('What book would you like to add?');
-    bookObj = new Book(bookName);
-    myLibrary.push(newBook);
+  //variables to store form input
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 // This function will loop through the array and display each book.
 function displayBooks(){
-    for (let i = 0; i < myLibrary.length; i++)
-        console.log(myLibrary[i]);
+    for (let i = 0; i < myLibrary.length; i++){
+      let book = document.createElement('li');
+      let removeBtn = document.createElement('button');
+      let readBtn = document.createElement('button'); //This button will read/unread the book.
+
+      book.textContent = myLibrary[i].info();
+      removeBtn.textContent = 'Remove';
+      readBtn.textContent = 'Read/Unread';
+
+      book.appendChild(removeBtn);
+      book.appendChild(readBtn);
+      
+      library.appendChild(book);
+    }
 }
 
 //When button clicked, form brought up where user input book info (title, author, pages, read)
+const newButton = document.querySelector('.new');
+const form = document.querySelector('.form');
+const addButton = document.querySelector('.add');
+const library = document.querySelector('.library');
+const allBooks = document.querySelectorAll('.library li');
+
+
+newButton.addEventListener('click', function(){
+  form.style.display = 'block';
+});
+
+addButton.addEventListener('click', () => {
+  console.log(allBooks);
+  addBookToLibrary();
+  displayBooks();
+});
+
+
+
